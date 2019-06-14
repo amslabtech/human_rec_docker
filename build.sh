e6380ddb4bdf7266676a0bee060e7017e5c58b1c
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-06-14 19:42:56 +0900

    first commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t human_rec_docker ./docker/
---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "=== run_docker ==="
+
+IMAGE_NAME=human_rec_docker
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+docker run -it --rm \
+  --net='host' \
+  --name="human_rec_docker" \
+  --volume="$SCRIPT_DIR/:/root/catkin_ws/src/$IMAGE_NAME/" \
+  $IMAGE_NAME \
+  bash -c "source /root/catkin_ws/devel/setup.bash;cd catkin_ws && catkin_make;roslaunch /root/catkin_ws/src/human_rec_docker/human_rec.launch"
+
