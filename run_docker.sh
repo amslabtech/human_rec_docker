5cc884e4c183e3e222dd728d328e0d57abb3b112
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-08-06 23:23:12 +0900

    update

--- run_docker.sh
+++ run_docker.sh
@@ -8,7 +8,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 docker run -it --rm \
   --net='host' \
   --name="human_rec_docker" \
-  --volume="$SCRIPT_DIR/:/root/catkin_ws/src/$IMAGE_NAME/" \
+  --volume="$SCRIPT_DIR/:/root/$IMAGE_NAME/" \
   $IMAGE_NAME \
-  bash -c "source /root/catkin_ws/devel/setup.bash;cd catkin_ws && catkin_make;roslaunch /root/catkin_ws/src/human_rec_docker/human_rec.launch"
-
+  bash
+  # roslaunch /root/$IMAGE_NAME/human_rec.launch
+  # bash -c "source /root/catkin_ws/devel/setup.bash;cd catkin_ws && catkin_make;roslaunch /root/catkin_ws/src/human_rec_docker/human_rec.launch"
