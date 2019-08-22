efd76cc3f9e18a4f629dc69e4a60e22638b9b72c
kazuki takahashi <tkhsh.schoen@gmail.com>
2019-08-22 17:38:07 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # human_rec_docker
+
+# How to use
+```
+for getting human position
+./run_docker.sh "roslaunch human_detection human_pos_publisher.launch"
+```
--- run_docker.sh
+++ run_docker.sh
@@ -10,6 +10,6 @@ docker run -it --rm \
   --name="human_rec_docker" \
   --volume="$SCRIPT_DIR/:/root/$IMAGE_NAME/" \
   $IMAGE_NAME \
-  bash
+  bash -c "$@"
   # roslaunch /root/$IMAGE_NAME/human_rec.launch
   # bash -c "source /root/catkin_ws/devel/setup.bash;cd catkin_ws && catkin_make;roslaunch /root/catkin_ws/src/human_rec_docker/human_rec.launch"
